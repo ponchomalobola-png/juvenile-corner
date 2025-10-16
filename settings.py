7defa3fe73b36df7f75bcb6fc454925130748fb0
Poncho Malobola <poncho.malobola@gmail.com>
2025-10-16 21:18:53 +0200

    Prepare for Render deployment

--- settings.py
+++ settings.py
@@ -2,8 +2,11 @@ import os
 
 # Static files
 STATIC_URL = '/static/'
+STATIC_DIR = 'static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files
 MEDIA_URL = '/media/'
+MEDIA_DIR = 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
